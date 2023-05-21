9950b09e718fdfb62f6f914de942d05994b98f13
RidGud <hoquangdat123@gmail.com>
2023-05-21 13:04:29 +0700

    Sắp lại file và link html

---
+++ src/test/java/LaptopWebsite/Nhom14/Nhom14ApplicationTests.java
@@ -0,0 +1,13 @@
+package LaptopWebsite.Nhom14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
