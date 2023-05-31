47baab59f97ab47eb26016f115540920b316f22b
RidGud <hoquangdat123@gmail.com>
2023-05-31 19:36:34 +0700

    Delete Repository and change a bit in Product

--- src/main/java/LaptopWebsite/Nhom14/Model/Product.java
+++ src/main/java/LaptopWebsite/Nhom14/Model/Product.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -22,15 +20,11 @@ public class Product {
     private String productName;
 
     @Column
-    private String image;
-
-    @Column
     private Double price;
 
     @Column
     private String description;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
+    
+    @Column
+    private String image;
 }
